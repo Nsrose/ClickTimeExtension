621569bff0562cc25426af6a26622db281ac6c8d
Nick Rose <nrose@clicktime.com>
2015-06-17 13:56:39 -0700

    clients list

--- js/app/PageController.js
+++ js/app/PageController.js
@@ -11,6 +11,26 @@ myApp.controller("PageController", ['$scope', 'APIService', '$http',
             var session = items['session'];
             if (session != null) {
                 // Everything's good to go here
+
+                $scope.UserName = session.UserName;
+                $scope.UserID = session.UserID;
+                $scope.CompanyID = session.CompanyID;
+                $scope.UserEmail = session.UserEmail;
+                $scope.Token = session.Token;
+
+                $scope.ClientID = '1';
+
+                // Fetch the clients
+                var clientsURL = API_BASE + "Companies/" + $scope.CompanyID + "/Users/" + 
+                    $scope.UserID + "/Clients";
+                APIService.apiCall(clientsURL, $scope.UserEmail, $scope.Token, "GET", function (data) {
+                    if (data == null) {
+                        alert("We're sorry, there was an error fetching the clients list.");
+                    }
+                    $scope.clients = data;
+                })
+                
+                
                 return;
             }
         }

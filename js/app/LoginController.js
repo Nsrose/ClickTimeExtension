2b5ac93c0b14191340838503d1af4de7c8101685
Yuan Yuan <yyuan@clicktime.com>
2015-06-17 17:14:09 -0700

    email validation and also fixed all your crappy indents

--- js/app/LoginController.js
+++ js/app/LoginController.js
@@ -1,27 +1,25 @@
-// Login controller. 
+// Login controller
 myApp.controller("LoginController", ['$scope', 'APIService', '$http', 
-	function ($scope, APIService, $http) {
+    function ($scope, APIService, $http) {
 
-	// Get the session, if it exists, go to popup. Otherwise, stay here.
+    // Get the session, if it exists, go to popup. Otherwise, stay here.
     chrome.storage.sync.get('session', function (items) {
-    	if ('session' in items) {
-    		// Session variable exists
-    	 	var session = items['session'];
-        	if (session != null) {
-            	// User does not need to login.
-            	window.location.href = "../../templates/popup.html";
-        	}
-    	}
+        if ('session' in items) {
+            // Session variable exists
+            var session = items['session'];
+            if (session != null) {
+                // User does not need to login.
+                window.location.href = "../../templates/popup.html";
+            }
+        }
     })
 
-
-	$scope.login = function (user) {
-		var sessionURL = API_BASE + "Session";
-		// Get the session for the user. If it exists, store it in local storage.
+    $scope.login = function (user) {
+        var sessionURL = API_BASE + "Session";
+        // Get the session for the user. If it exists, store it in local storage.
         APIService.apiCall(sessionURL, user.email, user.password, 'GET',
             function (session) {
-                if (session == null) {
-                	// bad request
+                if (session == null) { //bad request
                     alert(REQUEST_ERROR_MESSAGE);
                     return;
                 }
@@ -31,7 +29,8 @@ myApp.controller("LoginController", ['$scope', 'APIService', '$http',
                     console.log("Set session in local storage.");
                     window.location.href = "../../templates/popup.html";
                 })
-            })
+            }
+        )
     }
-
 }])
+

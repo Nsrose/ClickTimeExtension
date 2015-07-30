3860384a4c1c8dd2d2bb640794ec156a2d04f0f4
Nick Rose <nrose@clicktime.com>
2015-07-30 16:37:05 -0700

    persist environment change

--- js/app/AppController.js
+++ js/app/AppController.js
@@ -9,6 +9,18 @@ myApp.controller("AppController", ['$scope', '$location', '$apiBases', '$apiBase
         $scope.pageReady = false;
     })
 
+    // API base change upon opening:
+    chrome.storage.sync.get('apiBaseURL', function (items) {
+    	if ('apiBaseURL' in items) {
+    		$apiBase.url = items.apiBaseURL;
+    		chrome.runtime.sendMessage({
+    			changeUrl: true,
+    			url: $apiBase.url
+    		})
+    		$scope.$apply();
+    	}
+    })
+
     $scope.environmentButton = {
     	'count' : 0
     }
@@ -21,16 +33,23 @@ myApp.controller("AppController", ['$scope', '$location', '$apiBases', '$apiBase
     }
 
     $scope.$on('environmentChange', function (event, environment) {
+    	$scope.changeEnvironment(environment);
+    })
+
+    $scope.changeEnvironment = function (environment) {
     	if (environment in $apiBases) {
     		$apiBase.url = $apiBases[environment];
     		chrome.runtime.sendMessage({
     			changeUrl: true,
     			url: $apiBase.url
     		})
+    		chrome.storage.sync.set({
+    			'apiBaseURL' : $apiBase.url
+    		})
     	} else {
     		bootbox.alert("Invalid environment switch option: " + environment);
     	}
-    })
+    }
 
 
 }])

445b6bd39e93c5b7fb14d2b9beb1802b3fa1420e
ajones620 <jalexander620@gmail.com>
2015-08-11 13:46:58 -0700

    removed my name from files, updated allow reminder checkbox styling

--- js/lib/customSelect.js
+++ js/lib/customSelect.js
@@ -10,7 +10,7 @@
 
 	myApp.value('customSelectDefaults', {
 		displayText: 'Select...',
-		emptyListText: 'There are no items to display',
+		emptyListText: 'There are no items to display.',
 		emptySearchResultText: 'No results match "$0"',
 		addText: 'Add',
 		searchDelay: 1000
